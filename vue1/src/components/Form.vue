d94b3ea60983200aae3a19acfa130112849a60c7
sky0621 <sky14work@gmail.com>
2019-10-13 16:34:39 +0900

    continue

---
+++ vue1/src/components/Form.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    data: () => ({
+        defaultDateLabel: "1980-01-01",
+        dateLabel: null as string | null
+    }),
+    computed: {
+        dateFromDateLabel(): Date {
+            let label = this.defaultDateLabel
+            if (this.dateLabel !== null) {
+                label = this.dateLabel
+            }
+            return new Date(label)
+        }
+    }
+})
+</script>
---
+++ vue1/src/components/HelloWorld2.vue
@@ -0,0 +1,37 @@
+<template>
+    <div @click="onClickElement">
+        test
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export type Rect = {
+    width: number
+    height: number
+}
+
+export type Position = {
+    top: number
+    left: number
+}
+
+export default Vue.extend({
+    data: () => ({
+        bounds: null as ClientRect | DOMRect | null,
+    }),
+    methods: {
+        onClickElement({ target }: { target: HTMLElement }): void {
+            this.bounds = target.getBoundingClientRect()
+        }
+    },
+    computed: {
+        boundsInfo(): string {
+            if (this.bounds === null) return ""
+            return JSON.stringify(this.bounds)
+        },
+        // TODO: 
+    }
+})
+</script>
