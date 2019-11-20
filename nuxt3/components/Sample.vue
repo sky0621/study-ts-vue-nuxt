bd4a2f8ac98ccc0772ccaf30ec7d59c569ba763b
sky0621 <sky14work@gmail.com>
2019-11-21 00:50:13 +0900

    continue

---
+++ nuxt3/components/Sample.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>Name: {{ fullName }}</div>
+    <div>Message: {{ message }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      user: {
+        type: Object,
+        required: true,
+        default: () => ({
+          firstName: '',
+          lastName: 0
+        })
+      }
+    },
+
+    data() {
+      return {
+        message: 'This is a message'
+      }
+    },
+
+    computed: {
+      fullName() {
+        return this.$props.user.firstName + " " + this.$props.user.lastName
+      }
+    }
+  }
+</script>
---
+++ nuxt3/pages/sample.vue
@@ -0,0 +1,13 @@
+<template>
+  <Sample v-bind:user="{firstName: 'Sato', lastName: 123}" />
+</template>
+
+<script>
+  import Sample from '~/components/Sample.vue'
+
+  export default {
+    components: {
+      Sample
+    }
+  }
+</script>
