42b64dc6bf275c3b35ec737c0c2a7f6cad58e09f
sky0621 <sky14work@gmail.com>
2019-11-23 23:23:16 +0900

    temp

--- nuxt3/nuxt.config.js
+++ nuxt3/nuxt.config.js
@@ -39,6 +39,7 @@ export default {
   ** Nuxt.js modules
   */
   modules: [
+    '~/modules/typescript.js'
   ],
   /*
   ** Build configuration
--- nuxt3/pages/sample.vue
+++ nuxt3/pages/sample.vue
@@ -3,8 +3,8 @@
 </template>
 
 <script lang="ts">
-  import { Vue, Component } from 'nuxt-property-decorator'
-  import Sample from "~/components/Sample.vue";
+  import { Component, Vue } from 'nuxt-property-decorator'
+  import Sample from '@/components/Sample.vue'
 
   @Component({ components: { Sample } })
   export default class SamplePage extends Vue {}
